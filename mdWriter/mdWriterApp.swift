2530426751ca31cbcfad2c868feb437528cffab0
DeepPurple101 <adam.oravec0@gmail.com>
2022-12-21 22:12:17 +0100

    Initial Commit

---
+++ mdWriter/mdWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdWriterApp.swift
+//  mdWriter
+//
+//  Created by Adam Oravec on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct mdWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
